aa5af04f9f33ee0ac0cc4a7f5db4ce05934dbd1a
Alaa Alkheder <alaa-alkheder@outlook.com>
2024-03-18 19:36:17 +0300

    edit eslint

--- src/config/allConfig.type.ts
+++ src/config/allConfig.type.ts
@@ -1,5 +1,7 @@
 import { AppConfig } from '@config/app-config/app.type';
+import { SwaggerConfig } from "@config/swagger-config/swagger.type";
 
 export type AllConfigType = {
   app: AppConfig;
+  swagger:SwaggerConfig
 };
--- src/plugins/swagger/swagger.ts
+++ src/plugins/swagger/swagger.ts
@@ -1,81 +1,79 @@
-import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestApplication } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SwaggerConfig } from "@config/swagger-config/swagger.type";
-
-// import { ResponseDefaultSerialization } from 'src/common/response/serializations/response.default.serialization';
-// import { ResponsePagingSerialization } from 'src/common/response/serializations/response.paging.serialization';
-
+import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestApplication } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AllConfigType } from "@config/allConfig.type";
+import { writeFileSync } from "fs";
+import { ResponseDefaultSerialization } from "../../common/response/response.default.serialization";
+import { ResponsePagingSerialization } from "../../common/response/response.paging.serialization";
 
 
 export default async function (app: NestApplication) {
-  const configService = app.get(ConfigService<SwaggerConfig>);
+  const configService = app.get(ConfigService<AllConfigType>);
   const logger = new Logger();
 
-  // const env: string = configService.getOrThrow('nodeEnv', { infer: true });
-  const docName: string = configService.getOrThrow('docName', {
+  const env: string = configService.getOrThrow('app.nodeEnv', { infer: true });
+  const docName: string = configService.getOrThrow('swagger.docName', {
     infer: true,
   });
-  console.log(docName);
 
-  // const docDesc: string = configService.getOrThrow('docDesc', {
-  //   infer: true,
-  // });
-  // const docVersion: string = configService.getOrThrow('docVersion', {
-  //   infer: true,
-  // });
-  // const docPrefix: string = configService.getOrThrow('docPrefix', {
-  //   infer: true,
-  // });
-  //
-  // if (env !== 'production') {
-  //   const documentBuild = new DocumentBuilder()
-  //     .setTitle(docName)
-  //     .setDescription(docDesc)
-  //     .setVersion(docVersion)
-  //     .addServer('/')
-  //     .addBearerAuth(
-  //       { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-  //       'accessToken',
-  //     )
-  //     .addBearerAuth(
-  //       { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-  //       'refreshToken',
-  //     )
-  //     .addBearerAuth(
-  //       { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-  //       'google',
-  //     )
-  //     .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'apiKey')
-  //     .build();
-  //
-  //   const document = SwaggerModule.createDocument(app, documentBuild, {
-  //     deepScanRoutes: true,
-  //     extraModels: [ResponseDefaultSerialization, ResponsePagingSerialization],
-  //   });
-  //
-  //   writeFileSync('./data/json', JSON.stringify(document));
-  //   SwaggerModule.setup(docPrefix, app, document, {
-  //     jsonDocumentUrl: `${docPrefix}/json`,
-  //     yamlDocumentUrl: `${docPrefix}/yaml`,
-  //     customSiteTitle: docName,
-  //     swaggerOptions: {
-  //       docExpansion: 'none',
-  //       persistAuthorization: true,
-  //       displayOperationId: true,
-  //       operationsSorter: 'method',
-  //       tagsSorter: 'alpha',
-  //       tryItOutEnabled: true,
-  //       filter: true,
-  //       deepLinking: true,
-  //     },
-  //   });
-  //
-  //   logger.log(`==========================================================`);
-  //
-  //   logger.log(`Docs will serve on ${docPrefix}`, 'NestApplication');
-  //
-  //   logger.log(`==========================================================`);
-  // }
+  const docDesc: string = configService.getOrThrow('swagger.docDesc', {
+    infer: true,
+  });
+  const docVersion: string = configService.getOrThrow('swagger.docVersion', {
+    infer: true,
+  });
+  const docPrefix: string = configService.getOrThrow('swagger.docPrefix', {
+    infer: true,
+  });
+
+  if (env !== 'production') {
+    const documentBuild = new DocumentBuilder()
+      .setTitle(docName)
+      .setDescription(docDesc)
+      .setVersion(docVersion)
+      .addServer('/')
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'accessToken',
+      )
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'refreshToken',
+      )
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'google',
+      )
+      .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'apiKey')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, documentBuild, {
+      deepScanRoutes: true,
+      extraModels: [ResponseDefaultSerialization, ResponsePagingSerialization],
+    });
+
+    writeFileSync('./data/json', JSON.stringify(document));
+    SwaggerModule.setup(docPrefix, app, document, {
+      jsonDocumentUrl: `${docPrefix}/json`,
+      yamlDocumentUrl: `${docPrefix}/yaml`,
+      customSiteTitle: docName,
+      swaggerOptions: {
+        docExpansion: 'none',
+        persistAuthorization: true,
+        displayOperationId: true,
+        operationsSorter: 'method',
+        tagsSorter: 'alpha',
+        tryItOutEnabled: true,
+        filter: true,
+        deepLinking: true,
+      },
+    });
+
+    logger.log(`==========================================================`);
+
+    logger.log(`Docs will serve on ${docPrefix}`, 'NestApplication');
+
+    logger.log(`==========================================================`);
+  }
 }
